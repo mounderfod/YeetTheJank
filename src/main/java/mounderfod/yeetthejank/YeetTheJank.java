8ef707ad0b88b69842c0e8e9f5e185164c854d6a
redcreeper14385 <mounderfod@gmail.com>
2021-04-07 16:54:22 +0100

    Add log message

--- src/main/java/mounderfod/yeetthejank/YeetTheJank.java
+++ src/main/java/mounderfod/yeetthejank/YeetTheJank.java
@@ -6,6 +6,7 @@ public class YeetTheJank implements ModInitializer {
 
     @Override
     public void onInitialize() {
-
+        System.out.println("Yeeting the jank...");
+        System.out.println("Jank yeeted! Enjoy!");
     }
 }
