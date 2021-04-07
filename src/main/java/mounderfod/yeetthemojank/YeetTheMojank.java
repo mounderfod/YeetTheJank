3b0a16643b82347044708c9afe9ef78c27c1f370
redcreeper14385 <mounderfod@gmail.com>
2021-04-07 12:56:31 +0100

    Initial code commit

---
+++ src/main/java/mounderfod/yeetthemojank/YeetTheMojank.java
@@ -0,0 +1,4 @@
+package mounderfod.yeetthemojank;
+
+public class YeetTheMojank {
+}
---
+++ src/main/java/mounderfod/yeetthemojank/client/YeetTheMojankClient.java
@@ -0,0 +1,13 @@
+package mounderfod.yeetthemojank.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class YeetTheMojankClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
