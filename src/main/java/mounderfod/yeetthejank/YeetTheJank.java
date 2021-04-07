9d515d6018456a43ea59efacddcbb4ac2f50466a
redcreeper14385 <mounderfod@gmail.com>
2021-04-07 16:24:39 +0100

    Optimise imports in YeetTheJank.java

--- src/main/java/mounderfod/yeetthejank/YeetTheJank.java
+++ src/main/java/mounderfod/yeetthejank/YeetTheJank.java
@@ -1,7 +1,6 @@
 package mounderfod.yeetthejank;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.passive.WanderingTraderEntity;
 
 public class YeetTheJank implements ModInitializer {
 
